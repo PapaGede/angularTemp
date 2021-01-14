743225831a1f76b243eb674550a01f60b69731b9
PapaGede <naqhouamy@gmail.com>
2021-01-14 10:36:11 +0000

    something new

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/log-in/log-in.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
